e4edf7d97abc4c7520ba6b5b877aa16a07052944
Jayden Rasband <66230914+jrasband-dev@users.noreply.github.com>
2024-12-22 22:46:28 -0700

    PEP Compliant Changes

--- polars_extensions/name.py
+++ polars_extensions/name.py
@@ -9,54 +9,56 @@ class NameExtensionNameSpace:
 
     def to_pascal_case(self) -> pl.DataFrame:
         def _to_pascal_case(name: str) -> str:
-            return ''.join(word.capitalize() for word in re.sub(r'[_\s]+', ' ', name).split())
+            return "".join(
+                word.capitalize() for word in re.sub(r"[_\s]+", " ", name).split()
+            )
 
         columns = self._df.columns
         new_columns = {col: _to_pascal_case(col) for col in columns}
         return self._df.rename(new_columns)
 
-
     def to_snake_case(self) -> pl.DataFrame:
         def _to_snake_case(name: str) -> str:
-            return re.sub(r'(?<!^)(?=[A-Z])', '_', name).lower()
+            return re.sub(r"(?<!^)(?=[A-Z])", "_", name).lower()
+
         new_columns = {col: _to_snake_case(col) for col in self._df.columns}
         return self._df.rename(new_columns)
-    
+
     def to_camel_case(self) -> pl.DataFrame:
         def _to_camel_case(name: str) -> str:
-            words = re.sub(r'[_\s]+', ' ', name).split()
-            return words[0].lower() + ''.join(word.capitalize() for word in words[1:])
-        
+            words = re.sub(r"[_\s]+", " ", name).split()
+            return words[0].lower() + "".join(word.capitalize() for word in words[1:])
+
         new_columns = {col: _to_camel_case(col) for col in self._df.columns}
         return self._df.rename(new_columns)
 
     def to_pascal_snake_case(self) -> pl.DataFrame:
         def _to_pascal_snake_case(name: str) -> str:
-            words = re.sub(r'[_\s]+', ' ', name).split()
-            return '_'.join(word.capitalize() for word in words)
+            words = re.sub(r"[_\s]+", " ", name).split()
+            return "_".join(word.capitalize() for word in words)
+
         new_columns = {col: _to_pascal_snake_case(col) for col in self._df.columns}
         return self._df.rename(new_columns)
-    
 
     def to_kebeb_case(self) -> pl.DataFrame:
         def _to_kebeb_case(name: str) -> str:
-            return re.sub(r'(?<!^)(?=[A-Z])', '-', name).lower().replace('_', '-')
-        
+            return re.sub(r"(?<!^)(?=[A-Z])", "-", name).lower().replace("_", "-")
+
         new_columns = {col: _to_kebeb_case(col) for col in self._df.columns}
         return self._df.rename(new_columns)
 
-    
     def to_upper_snake_case(self) -> pl.DataFrame:
         def _to_upper_snake_case(name: str) -> str:
-            return re.sub(r'(?<!^)(?=[A-Z])', '_', name).upper().replace('-', '_')
-        
+            return re.sub(r"(?<!^)(?=[A-Z])", "_", name).upper().replace("-", "_")
+
         new_columns = {col: _to_upper_snake_case(col) for col in self._df.columns}
         return self._df.rename(new_columns)
 
-
     def to_train_case(self) -> pl.DataFrame:
         def _to_train_case(name: str) -> str:
-            return '-'.join(word.capitalize() for word in re.sub(r'[_\s]+', ' ', name).split())
-        
+            return "-".join(
+                word.capitalize() for word in re.sub(r"[_\s]+", " ", name).split()
+            )
+
         new_columns = {col: _to_train_case(col) for col in self._df.columns}
         return self._df.rename(new_columns)
