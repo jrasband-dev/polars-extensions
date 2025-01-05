a69dca615455584da955c9014e72f0af1e3ff67a
Jayden Rasband <66230914+jrasband-dev@users.noreply.github.com>
2025-01-05 00:58:26 -0700

    Release Updates

--- polars_extensions/__init__.py
+++ polars_extensions/__init__.py
@@ -1,13 +1,9 @@
 from .io import *
 from .name import *
-from .string import *
-from .list import *
 from .numeric import *
 
 __all__ = [
     "NameExtensionNameSpace",
-    "StringExtensionNamespace",
-    "ListExtensionNamespace",
     "NumericExtensionNamespace",
     "write_schema",
     "read_schema",
