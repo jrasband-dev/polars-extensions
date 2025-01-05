c34c22bfe3334ce1386be5ad6314ec19e53797e6
Jayden Rasband <66230914+jrasband-dev@users.noreply.github.com>
2025-01-05 00:54:21 -0700

    Added numeric module

--- polars_extensions/__init__.py
+++ polars_extensions/__init__.py
@@ -1,10 +1,14 @@
 from .io import *
 from .name import *
 from .string import *
+from .list import *
+from .numeric import *
 
 __all__ = [
     "NameExtensionNameSpace",
     "StringExtensionNamespace",
+    "ListExtensionNamespace",
+    "NumericExtensionNamespace",
     "write_schema",
     "read_schema",
 ]
