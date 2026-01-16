50676a500f9bebe64c1be520bb6b272dd5384aeb
jrasband-dev <jayden.rasband@gmail.com>
2026-01-15 17:10:54 -0700

    bumped version

--- polars_extensions/__init__.py
+++ polars_extensions/__init__.py
@@ -10,7 +10,7 @@ from .technical_analysis import TechnicalAnalysisNamespace
 from .mssql.writer import MSSQLNamespace
 
 
-__version__ = "1.4.0"
+__version__ = "1.4.1"
 
 __all__ = [
     "NameExtensionNameSpace",
